808003d4986b70418f852108b11120c88db460bd
kipngeno kevin <kipngenokevin254@gmail.com>
2023-10-18 06:01:10 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a playground for the project on Monty Interpreter
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - receives file inputs from a .m file
+ * @argc: intger count of the arguments passed
+ * @argv: array of strings passed
+ * Return: 0 success
+ */
+int main(int argc, char **argv)
+{
+	return (0);
+}
