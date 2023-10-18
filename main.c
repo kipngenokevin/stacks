1a3c66810aef40e2446933cc57dd955f0870c577
kipngeno kevin <kipngenokevin254@gmail.com>
2023-10-18 13:32:20 -0700

    Added the instructions into an array

--- main.c
+++ main.c
@@ -7,5 +7,34 @@
  */
 int main(int argc, char **argv)
 {
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop}
+	};
+	int i, line_no, opcode_found = 0;
+	int size_of_instruction = sizeof(instructions) / sizeof(instructions[0]);
+	stack_t *stack = NULL;
+
+	/* Function that loops throug the file to find opcode and line numbers */
+	/* on obtaining opcode, pass it to the necessary function */
+	for (i = 0; i < size_of_instruction; i++)
+	{
+		if (strcmp(opcode, instructions[i].opcode == 0))
+		{
+			instructions[i].f(&stack, line_no);
+			opcode_found = 1;
+			break;
+		}
+	}
+	if (!opcode_found)
+	{
+		fprintf(stderr, "L<line_number>: unknown instruction <opcode>\n");
+		exit(EXIT_FAILURE);
+	}
 	return (0);
 }
--- monty.h
+++ monty.h
@@ -1,5 +1,8 @@
 #ifndef MONTY_H
 #define MONTY_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -28,4 +31,10 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
 #endif
